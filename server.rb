8599ea093a0dd18c31ae96cfe02580637e52ad9b
marcosgabrielserrano <marcos12@mail.usf.edu>
2018-11-29 12:08:03 -0500

    Finally, got connection to work

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'mysql2'
+require 'csv'
 require 'sinatra/reloader' if development?
 # password to sql marcoserika
 # #mysql 5.7 from official wesite
@@ -12,8 +13,9 @@ end
 
 get '/main.html' do
   print 'main'
+  create_db
+  connect_sql
   File.read(File.join('main.html'))
-  print connect_sql
 end
 
 not_found do
@@ -22,7 +24,51 @@ end
 
 
 def connect_sql()
+  print("")
   #db = Mysql2.connect('localhost', 'marcos', 'H@ha12345', 'db1')
-  mysql = Mysql2::Client.new(:host => 'localhost', :username => 'root', :password => 'H@ha12345')
+  mysql = Mysql2::Client.new(:host => 'localhost', :username => 'root', :password => 'H@ha12345', :database => 'test')
+  results = mysql.query("SELECT * FROM userpass")
+  results.each do |row|
+    print("\n"+(row.to_s))
+  end
   '\nSuccess\n'
+end
+
+
+def create_db()
+  #connect to db first
+  db = Mysql2::Client.new(:host => 'localhost', :username => 'root', :password => 'H@ha12345', :database => 'test')
+
+  #Read csv for data to fill the dbs
+  CSV.foreach("information.csv") do |row|
+
+    qry = "SELECT EXISTS (SELECT * FROM userpass WHERE user = '" + row[0] +"')"
+    results = db.query(qry)
+    exists = false
+    results.each do |row|
+      print("\n" + (row.to_s) +"\n")
+      if row.to_s.include? "=>1"
+        exists = true
+      end
+    end
+    if exists == true
+      print("\nCONTINUING\n")
+      next
+    end
+    #updating usernames and passwords table
+    qry = "INSERT INTO userpass (user, pass) VALUES ('" + row[0] + "', '" + row[1] + "')"
+    results = db.query(qry)
+    print(results)
+
+    #updating roles table
+    qry = "INSERT INTO roles (user, role) VALUES ('" + row[0] + "', '" + row[2] + "')"
+    results = db.query(qry)
+    print(results)
+
+    if row[2] == 'instructor'
+      print('instructor')
+    else
+      print('student')
+    end
+  end
 end
